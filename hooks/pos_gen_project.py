7148dc3224df26993d1c151a5de18ddec5b99209
pablo-figueroa <pablo.figueroa.7once@gmail.com>
2024-01-16 13:25:02 -0300

    probando 1

--- hooks/pos_gen_project.py
+++ hooks/pos_gen_project.py
@@ -6,18 +6,19 @@ RESET_ALL = r"\x1b[0m"
 print(f"{MESSAGE_COLOR}Almost done!")
 print(f"Initializing a git repository...{RESET_ALL}")
 
-# Subproceso para instalar las librerias en requirements.txt
-# subprocess.call(['pip', 'install', '-r', 'requirements.txt'])
-
-#Con conda
-#subprocess.call(['conda', 'env', 'create','--file','environment.yml'])
-
-#subprocess.call(["conda", "create", "-n", "{{cookiecutter.environment_name}}"])
-
-#subprocess.call(["mamba", "env", "update", "-n", "{{cookiecutter.environment_name}}", "-f", "environment.yml"])
-
 subprocess.call(["git", "init"])
 subprocess.call(["git", "add", "*"])
 subprocess.call(["git", "commit", "-m", "Initial commit"])
+subprocess.call(["git", "branch", "-M", "main"])
+
+#Con conda
+subprocess.call(['conda', 'env', 'create','--file','environment.yml'])
+subprocess.call(["conda", "activate", "{{cookiecutter.project_slug}}"])
+#subprocess.call(["conda", "create", "--name", "{{cookiecutter.environment_name}}"])
+
+#subprocess.call(["mamba", "env", "update", "-n", "{{cookiecutter.environment_name}}", "-f", "environment.yml"])
+
+# Subproceso para instalar las librerias en requirements.txt
+# subprocess.call(['pip', 'install', '-r', 'requirements.txt'])
 
 print(f"{MESSAGE_COLOR}The beginning of your destiny is defined now! Create and have fun!{RESET_ALL}")
