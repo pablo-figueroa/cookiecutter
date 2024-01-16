96bf59db32c840d9a6944a936ff5fc4828776178
pablo-figueroa <pablo.figueroa.7once@gmail.com>
2024-01-16 15:49:06 -0300

    vuelta al original

--- hooks/pos_gen_project.py
+++ hooks/pos_gen_project.py
@@ -9,16 +9,7 @@ print(f"Initializing a git repository...{RESET_ALL}")
 subprocess.call(["git", "init"])
 subprocess.call(["git", "add", "*"])
 subprocess.call(["git", "commit", "-m", "Initial commit"])
-subprocess.call(["git", "branch", "-M", "main"])
 
-#Con conda
-subprocess.call(['conda', 'env', 'create','--file','environment.yml'])
-subprocess.call(["conda", "activate", "{{cookiecutter.project_slug}}"])
-#subprocess.call(["conda", "create", "--name", "{{cookiecutter.environment_name}}"])
-
-#subprocess.call(["mamba", "env", "update", "-n", "{{cookiecutter.environment_name}}", "-f", "environment.yml"])
-
-# Subproceso para instalar las librerias en requirements.txt
-# subprocess.call(['pip', 'install', '-r', 'requirements.txt'])
-
-print(f"{MESSAGE_COLOR}The beginning of your destiny is defined now! Create and have fun!{RESET_ALL}")
+print(
+    f"{MESSAGE_COLOR}The beginning of your destiny is defined now! Create and have fun!{RESET_ALL}"
+)
--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,14 +1,9 @@
-# inicializar un reporte de git
-# verificar que un valor tenga el contenido adecuado
-
-#print('Hello')
-
 import os
 import sys
 
 PROJECT_SLUG = "{{ cookiecutter.project_slug }}"
 
-ERROR_COLOR = r"\x1b[31m" # To change the terminal color
+ERROR_COLOR = r"\x1b[31m"
 MESSAGE_COLOR = r"\x1b[34m"
 RESET_ALL = r"\x1b[0m"
 
@@ -17,5 +12,5 @@ if PROJECT_SLUG.startswith("x"):
 
     sys.exit(1)
 
-print(f"{MESSAGE_COLOR}Let's do it! You're going to create something awesome!")
+print(f"{MESSAGE_COLOR}Let's do it! You're are going to create something awesome!")
 print(f"Creating project at { os.getcwd() }{RESET_ALL}")
